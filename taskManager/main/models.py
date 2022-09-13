5592e40bcf872b74b3c1e7b1f6094b978ca08119
Artem <artemka757571@gmail.com>
2022-09-13 21:28:30 +0300

    Model task add

--- taskManager/main/models.py
+++ taskManager/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField('Title', max_length=60)
+    task = models.TextField('Task')
+    create_on = models.DateTimeField('Create on', auto_now_add=True)
+    author = models.CharField('Author', max_length=40)
+
+    def __str__(self):
+        return self.title
